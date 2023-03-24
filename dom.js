bbdb8a0a74c38e4eb4de097448c495349a5133de
Slimobedsama <ohwo.okoro@yahoo.com>
2023-03-24 11:47:45 +0100

    Querried the dom selectors

---
+++ dom.js
@@ -0,0 +1,9 @@
+const content = document.querySelector('div .content');
+const msg = document.querySelector('p .msg');
+const dataInput = document.querySelector('#figure');
+const currentRandom = document.querySelector('.current-dis-1');
+const currentGuess = document.querySelector('.current-dis-2');
+const previousRandomNum = document.querySelector('.previous-dis-1');
+const previousGuess = document.querySelector('.previous-dis-2');
+const enterButton = document.querySelector('div input[value="Enter"]');
+const clearButton = document.querySelector('div input[value="Clear"]');
