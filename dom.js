df5d40999488eb3dc090689cd7f1765503d298d4
Slimobedsama <ohwo.okoro@yahoo.com>
2023-03-24 22:38:25 +0100

    Moved the random number function into the game play function

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-//DOM QUERY
+//QUERIES THE DOM 
 const content = document.querySelector('div .content');
 const msg = document.querySelector('p .msg');
 const dataInput = document.querySelector('#figure');
@@ -8,13 +8,14 @@ const previousRandomNum = document.querySelector('.previous-dis-1');
 const previousGuess = document.querySelector('.previous-dis-2');
 const enterButton = document.querySelector('div input[value="Enter"]');
 const clearButton = document.querySelector('div input[value="Clear"]');
-// THIS DEFINES THE RANDOM NUMBER
-const randomNum = Math.floor(Math.random() * 100) + 1;
-// SETTING THE GAME PLAY ROUNDS COUNT
+
+// SETS THE GAME PLAY ROUNDS COUNT
 let gameRound = 1;
 
-// CREATING THE GAME PLAY FUNCTION
+// CREATES THE GAME PLAY FUNCTION
 function playGame() {
+    // THIS DEFINES THE RANDOM NUMBER
+    const randomNum = Math.floor(Math.random() * 100) + 1;
     currentRandom.textContent = `Current Random Number: ${randomNum}`;
     currentGuess.textContent = `Current Guess: ${dataInput.value}`;
 }
